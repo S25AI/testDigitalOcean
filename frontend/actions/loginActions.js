5b6c3fde8e5cfc1ad092020ee67ab1cdb59c3db0
S25AI <jade25st@gmail.com>
2019-04-08 01:01:56 +0300

    test(actions): add tests for loginActions

--- frontend/actions/loginActions.js
+++ frontend/actions/loginActions.js
@@ -1,6 +1,6 @@
+import axios from 'axios';
 import store from '../store';
-
-import {authInstance as authAxios, instance as axios} from '../config/axiosConfig';
+import Cookie from 'js-cookie';
 
 import {
   LOGIN_CHANGE_LOGIN,
@@ -9,12 +9,8 @@ import {
   LOGIN_DATA_SUCCESS,
   LOGIN_DATA_FAIL,
   LOGIN_AUTH_CONNECT,
-  LOGIN_AUTH_DISCONNECT,
-  LOGIN_AUTH_CHECK_LOADING,
-  LOGIN_AUTH_CHECK_COMPLETE
+  LOGIN_AUTH_DISCONNECT
 } from '../constants/loginConstants';
-
-import {API_REQUEST_AUTH, API_REQUEST_USER_AUTH_CHECK} from '../constants/API';
 
 export const changeLoginField = (val) => ({
   type: LOGIN_CHANGE_LOGIN,
@@ -32,31 +28,20 @@ export const connectAuth = (login) => ({
 });
 
 export const disconnectAuth = () => {
-  localStorage.removeItem('token');
-  localStorage.removeItem('userId');
+  Cookie.remove('login');
   return {type: LOGIN_AUTH_DISCONNECT};
 }
 
-export const checkUserAuth = () => (dispatch) => {
-  let userId = localStorage.getItem('userId');
-  if (!userId) {
-    return dispatch(disconnectAuth());
-  }
-
-  dispatch({type: LOGIN_AUTH_CHECK_LOADING});
-
-  authAxios.post(API_REQUEST_USER_AUTH_CHECK, {userId})
+export const checkCookie = (login) => (dispatch) => {
+  dispatch(connectAuth(login));
+  axios.post('http://localhost:9002/api/cookieCheck', {login})
     .then(response => {
-      dispatch(connectAuth({login: response.data.login}));
-      dispatch({type: LOGIN_AUTH_CHECK_COMPLETE})
+      if (response.data && response.data.message !== 'authorized') {
+        dispatch(disconnectAuth());
+      }
     })
-    .catch((err) => {
-      console.log('err is ', err);
-      dispatch({type: LOGIN_AUTH_CHECK_COMPLETE});
-      dispatch(disconnectAuth());
-    });
-
-};
+    .catch(console.error);
+}
 
 export const handleFormSubmit = (e) => {
   e.preventDefault();
@@ -67,21 +52,14 @@ export const handleFormSubmit = (e) => {
 
   return (dispatch) => {
     dispatch({type: LOGIN_DATA_REQUEST});
-    axios.post(API_REQUEST_AUTH, {username: login, password}, {withCredentials: true})
+    return axios.post('http://localhost:9002/api/auth', {login, password}, {withCredentials: true})
       .then(response => {
         dispatch({
           type: LOGIN_DATA_SUCCESS,
-          payload: {
-            token: response.data.token,
-            login: response.data.username,
-            userId: response.data._id
-          }
+          payload: response.data
         });
-        localStorage.setItem('token', response.data.token);
-        localStorage.setItem('userId', response.data._id);
       })
       .catch((err) => {
-        console.log('err is ', err);
         dispatch({
           type: LOGIN_DATA_FAIL,
           payload: err.response && err.response.data
